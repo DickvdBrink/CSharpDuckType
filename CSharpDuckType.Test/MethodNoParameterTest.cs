4b226a96bb4641f1825bd112818f4f80adf3ac3a
Dick van den Brink <d_vandenbrink@outlook.com>
2014-12-18 19:41:49 +0100

    Added test project

---
+++ CSharpDuckType.Test/MethodNoParameterTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpDuckType.Test
+{
+    [TestClass]
+    public class MethodNoParameterTest
+    {
+        interface Foo
+        {
+            void Bar();
+        }
+
+        class Baz : Foo
+        {
+            public void Bar()
+            {
+            }
+        }
+
+        [TestMethod]
+        public void EmptyMethodNoReturnType()
+        {
+            Baz baz = new Baz();
+            Foo foo = DuckType.Cast<Foo>(baz);
+            Assert.AreNotSame(baz, foo);
+            foo.Bar();
+        }
+    }
+}
