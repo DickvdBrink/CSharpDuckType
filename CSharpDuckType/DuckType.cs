e43a9702605afc88efa4df0df70d74319227ca3e
Dick van den Brink <d_vandenbrink@outlook.com>
2014-12-18 19:41:24 +0100

    Added empty project

---
+++ CSharpDuckType/DuckType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDuckType
+{
+    public class DuckType
+    {
+        public static T Cast<T>(object duck)
+        {
+            return default(T);
+        }
+    }
+}
