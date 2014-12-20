019363ea408eda7600f11d9407e5cf6489e57df2
Dick van den Brink <d_vandenbrink@outlook.com>
2014-12-20 18:36:24 +0100

    Added overloads test with the same return types but different parameters

--- CSharpDuckType.Test/MethodOverloadTest.cs
+++ CSharpDuckType.Test/MethodOverloadTest.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CSharpDuckType.Test
 {
     [TestClass]
-    public class InterfaceWithOverloadsTest
+    public class MethodOverloadTest
     {
         public interface Foo
         {
@@ -12,6 +12,9 @@ namespace CSharpDuckType.Test
             string Bar(string x);
             int Bar(int i);
             bool Bar(bool b);
+
+            string Qux(string x);
+            string Qux(int i);
         }
 
         public class Baz
@@ -26,10 +29,17 @@ namespace CSharpDuckType.Test
             public bool Bar(bool b) {
                 return !b;
             }
+
+            public string Qux(string x) {
+                return x;
+            }
+            public string Qux(int i) {
+                return i.ToString();
+            }
         }
 
         [TestMethod]
-        public void OverloadTest()
+        public void OverloadTestDifferentReturnTypes()
         {
             Baz baz = new Baz();
             Foo foo = DuckType.Cast<Foo>(baz);
@@ -38,6 +48,17 @@ namespace CSharpDuckType.Test
             Assert.AreEqual("Hello World!", foo.Bar("Hello"));
             Assert.AreEqual(4, foo.Bar(2));
             Assert.AreEqual(false, foo.Bar(true));
+        }
+
+        [TestMethod]
+        public void OverloadTestSameReturnTypes()
+        {
+            Baz baz = new Baz();
+            Foo foo = DuckType.Cast<Foo>(baz);
+            Assert.AreNotSame(baz, foo);
+
+            Assert.AreEqual("Hello World!", foo.Qux("Hello World!"));
+            Assert.AreEqual("2", foo.Qux(2));
         }
     }
 }
