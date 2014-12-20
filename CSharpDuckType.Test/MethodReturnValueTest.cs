9fe0eb1dc7ca75ea8da958e67e3c469b25b7dfeb
Dick van den Brink <d_vandenbrink@outlook.com>
2014-12-20 19:39:00 +0100

    Added new empty test files for multiple interfaces and generics

--- CSharpDuckType.Test/MethodReturnValueTest.cs
+++ CSharpDuckType.Test/MethodReturnValueTest.cs
@@ -31,7 +31,6 @@ namespace CSharpDuckType.Test
             Assert.AreNotSame(baz, foo);
         }
 
-
         [TestMethod]
         public void TestBool()
         {
---
+++ CSharpDuckType.Test/MethodWithGenericsTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpDuckType.Test
+{
+    [TestClass]
+    public class MethodWithGenericsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
